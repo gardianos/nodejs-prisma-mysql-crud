d5df884b37c927cff838db992048e003053020c9
gardianos <gardianii@hotmail.com>
2021-10-05 20:33:02 +0200

    get all products and categories

--- routes/api.route.js
+++ routes/api.route.js
@@ -5,8 +5,16 @@ const prisma = new PrismaClient();
 
 router.get("/products", async (req, res, next) => {
   try {
-    const products = await prisma.product.findMany({});
-    res.json(products);
+    const products = await prisma.product.findMany({
+      include: {
+        category: true,
+      },
+    });
+
+    const categories = await prisma.category.findMany({
+      include: { products: true },
+    });
+    res.json({ products, categories });
   } catch (error) {
     next(error);
   }
