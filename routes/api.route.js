283679af23c943bd7706c47cd7c0ed01068fe0fe
gardianos <gardianii@hotmail.com>
2021-10-07 11:50:53 +0200

    delete the product by id

--- routes/api.route.js
+++ routes/api.route.js
@@ -55,7 +55,19 @@ router.post("/products", async (req, res, next) => {
 
 //DELETE the product by id
 router.delete("/products/:id", async (req, res, next) => {
-  res.send({ message: "Ok api is working 🚀" });
+  try {
+    const { id } = req.params;
+
+    const deletedProduct = await prisma.product.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    res.json(deletedProduct);
+  } catch (error) {
+    next(error);
+  }
 });
 
 // UPDATE the product by id
